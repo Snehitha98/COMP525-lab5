3dbb33f919cceda5d8ebc5b6d4f3c8284b9592fd
snehitha <mamidisnehitha@gmail.com>
2020-03-23 22:18:56 -0400

    Implemented highest_salary_by_pos()

--- problems/nbatop.py
+++ problems/nbatop.py
@@ -10,10 +10,8 @@ class NBATop(object):
     """
     Illustrate the split-accumulate-combine pattern
     """
-    def __init__(self, dataset):
-        self.dataset = dataset
 
-    def highest_salary_by_pos(self):
+    def highest_salary_by_pos(self, nba_data):
         """
         Find the highest NBA player salary for each position.
         Salary and position information is in the dataset attribute.
@@ -23,13 +21,26 @@ class NBATop(object):
         """
         # To return the dictionary with positions as keys and highest salary as
         # integer, we define an empty dictionary. which we highest_pos_d
-        highest_pos_d = {}
-
-        return highest_pos_d
+        salaries = nba_data[1]
+        positions = nba_data[2]
+        salaries_by_pos = {}
+        for index in range(len(salaries)):
+            key = positions[index]
+            a_salary = salaries[index]
+            if key in salaries_by_pos:
+                salaries_by_pos[key] += [a_salary]
+            else:
+                salaries_by_pos[key] = [a_salary]
+        result = {}
+        for key in salaries_by_pos:
+            values = salaries_by_pos[key]
+            max_salary = max(values)
+            result[key] = max_salary
+        return result
 
 
 if __name__ == '__main__':
-    dataset = [['tom'], [1000], ['PG']]
-    p = NBATop(input)
-    result = p.highest_salary_by_pos()
+    nba_data = [['tom'], [1000], ['PG']]
+    p = NBATop()
+    result = p.highest_salary_by_pos(nba_data)
     print(f'Highest salary by position: {result}')
